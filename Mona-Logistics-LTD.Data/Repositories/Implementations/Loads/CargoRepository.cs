ff2288b97cbf69beec91e2826b8c77d7176adc55
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-06-02 21:53:59 +0300

    Add repository implementations for entities

---
+++ Mona-Logistics-LTD.Data/Repositories/Implementations/Loads/CargoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mona_Logistics_LTD.Data.Models.Loads;
+using Mona_Logistics_LTD.Data.Repositories.Implementations.Base;
+using Mona_Logistics_LTD.Data.Repositories.Interfaces.Loads;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mona_Logistics_LTD.Data.Repositories.Implementations.Loads;
+
+public class CargoRepository : RepositoryAsync<Cargo, Guid>, ICargoRepository
+{
+
+    private readonly AppDbContext _context;
+    public CargoRepository(AppDbContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+
+}
---
+++ Mona-Logistics-LTD.Data/Repositories/Implementations/Trucks/TruckRepository.cs
@@ -0,0 +1,19 @@
+﻿using Mona_Logistics_LTD.Data.Models.Trucks;
+using Mona_Logistics_LTD.Data.Repositories.Implementations.Base;
+using Mona_Logistics_LTD.Data.Repositories.Interfaces.Trucks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mona_Logistics_LTD.Data.Repositories.Implementations.Trucks;
+
+public class TruckRepository : RepositoryAsync<Truck, Guid>, ITruckRepository
+{
+    private readonly AppDbContext _context;
+    public TruckRepository(AppDbContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+{
+}
