60b32d17d65227d0da495c36c68b378ab3be83ff
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-07-09 20:54:39 +0300

    Added ChangeUserRoleViewModel

---
+++ Mona-Logistics-LTD.Web.ViewModels/Admin/Interactions/ChangeUserRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mona_Logistics_LTD.Web.ViewModels.Admin.Interactions;
+
+
+public class ChangeUserRoleViewModel
+{
+    public Guid UserId { get; set; }
+    public string NewRole { get; set; } = string.Empty;
+
+    public IEnumerable<string> AvailableRoles { get; set; } = new List<string>();
+}
