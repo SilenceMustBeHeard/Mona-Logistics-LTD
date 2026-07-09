f00c6ae90386da00385d4d08e7c060b4d5f93c68
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-07-10 01:31:04 +0300

    Added UserManagementController

---
+++ Mona-Logistics-LTD.Web/Areas/Admin/Controllers/Interactions/UserManagementController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Mona_Logistics_LTD.Data.Models.Base;
+using Mona_Logistics_LTD.Services.Admin.Interfaces.Interactios;
+using Mona_Logistics_LTD.Web.Areas.Admin.Controllers.Account;
+using Mona_Logistics_LTD.Web.ViewModels.Admin.Interactions;
+
+namespace Mona_Logistics_LTD.Web.Areas.Admin.Controllers.Interactions;
+
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class UserManagementController : BaseAdminController
+{
+    private readonly IUserManagementService _userService;
+
+    public UserManagementController(
+        IUserManagementService userService,
+        UserManager<AppUser> userManager) : base(userManager)
+    {
+        _userService = userService;
+    }
+
+    // gets all users with their roles except the  admin
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var allUsers = await _userService
+            .GetUserManagmentBoardDataAsync(this.GetUserId());
+
+        return View(allUsers);
+    }
+
+    // add role to user
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AssignRole(ChangeUserRoleViewModel model)
+    {
+        if (string.IsNullOrWhiteSpace(model.NewRole))
+        {
+            TempData["ErrorMessage"] = "Please select a valid role.";
+            return RedirectToAction("Index");
+        }
+
+        var result = await _userService.ChangeUserRoleAsync(model, this.GetUserId());
+
+        if (result.Failed)
+            TempData["ErrorMessage"] = result.ErrorMessage;
+        else
+            TempData["SuccessMessage"] = "User role changed successfully.";
+
+        return RedirectToAction("Index");
+    }
+
+    // Ban user (soft delete)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DisableUser(string userId)
+    {
+        var result = await _userService.DisableUser(userId);
+
+        if (result.Failed)
+            TempData["ErrorMessage"] = result.ErrorMessage;
+        else
+            TempData["SuccessMessage"] = "User disabled successfully.";
+
+        return RedirectToAction(nameof(Index));
+    }
+}
