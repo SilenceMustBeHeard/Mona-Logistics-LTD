d082b7e53a6e409c87899a9d6316ca91afdfbd39
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-07-20 05:17:35 +0300

    Add Admin Dashboard controller and adjust views
    Introduce an Admin DashboardController (Area: Admin) that inherits BaseAdminController and restricts access to Admin role; constructor injects UserManager<AppUser>. Rename admin view from Areas/Admin/Views/Home/Index.cshtml to Areas/Admin/Views/Dashboard/Index.cshtml to match the controller. Update the public Views/Home/Index.cshtml to comment out the hero badge (removed emoji) and remove the partners logo grid as part of a homepage UI cleanup.

---
+++ Mona-Logistics-LTD.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Mona_Logistics_LTD.Data.Models.Base;
+using Mona_Logistics_LTD.Web.Areas.Admin.Controllers.Account;
+
+namespace Mona_Logistics_LTD.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class DashboardController : BaseAdminController
+{
+    public DashboardController(UserManager<AppUser> userManager) : base(userManager)
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
